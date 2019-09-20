5b6b6e9f04869d26e44cf94f370dba2f9409ec98
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-20 14:34:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -88,9 +88,11 @@ def won?
       
     if @board[position1] == "X" && @board[position2] == "X" && @board[position3] =="X" 
       won = combo
+      break
     elsif
      if @board[position1] == "O" && @board[position2] == "O" && @board[position3] =="O" 
        won = combo
+       break
     else
       won = false
     end
