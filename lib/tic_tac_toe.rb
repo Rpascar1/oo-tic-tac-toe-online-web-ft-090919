fa04dc079c67e9a2c8d5abfcac560b7c61456e92
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-20 16:07:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -122,7 +122,7 @@ WIN_COMBINATIONS =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,
 
   def winner
     if won? == combo
-      current_player
+      current_player -=1
      end
     end
    
