3f8f12afcbb32e7bb10df115ae6cd8996abbb612
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-20 16:58:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -131,14 +131,9 @@ WIN_COMBINATIONS =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,
  end       
 
 def play
-  puts "Want to play a game..........#JIGSAW -  Tye 1-9"
-  input = gets
-  input = input.to_i
-  input -=1
-  puts @board[input]
-  3.times{over?}
+  unitl over?
   turn
-
+  end
 end
   
 
