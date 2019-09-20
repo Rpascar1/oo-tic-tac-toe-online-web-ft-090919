df5fda7d02e3c6bb9db7d848130170177952f6d0
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-20 16:36:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -133,7 +133,8 @@ WIN_COMBINATIONS =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,
 def play
   puts "Want to see the play? Tye 1-9"
   input = gets
-  input = input -=1
+  input.to_i
+  input -=1
   @board[input]
 end
   
