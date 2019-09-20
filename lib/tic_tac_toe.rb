e466253e914869ebf91ec77b5ffe4a942e72126c
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-20 16:10:37 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -8,8 +8,8 @@ WIN_COMBINATIONS =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,
     @@player = nil
 
   def initialize
-    @board = [" "," "," "," "," "," "," "," "," "]
-    board = @board
+    @@board = [" "," "," "," "," "," "," "," "," "]
+    board = @@board
   end  
 
   def display_board
