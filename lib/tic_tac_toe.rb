9bdfcb5a682803abc99117dea6a6b9260f3b0660
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-20 14:20:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -77,7 +77,7 @@ WIN_COMBINATIONS =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,
     end
   end
 
-  def won?(board)
+  def won?(@board)
     won  = nil
     
     WIN_COMBINATIONS.each do |combo|
