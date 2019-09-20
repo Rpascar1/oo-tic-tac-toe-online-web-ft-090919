7587d909d7ac1cf35ae5111326bb41795a7f839e
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-20 14:55:05 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,6 +9,7 @@ WIN_COMBINATIONS =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,
 
   def initialize
     @board = [" "," "," "," "," "," "," "," "," "]
+    board = @board
   end  
 
   def display_board
@@ -95,7 +96,7 @@ WIN_COMBINATIONS =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,
           break
           end
         
-         def full(@board)
+         def full(board)
            turn_count == 9
          end
         
