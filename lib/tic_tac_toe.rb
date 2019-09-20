2f97386c1c6005e77e3c12c65aa94253d13909b9
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-20 12:59:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -69,7 +69,7 @@ WIN_COMBINATIONS =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,
       move(move_down,player)
       display_board
     else
-       turn
+      turn
     end
   end
   
