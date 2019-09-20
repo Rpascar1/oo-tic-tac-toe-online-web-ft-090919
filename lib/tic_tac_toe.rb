bb7651df89468bd9dd689e4e60944188e140c0de
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-20 15:08:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -106,6 +106,7 @@ WIN_COMBINATIONS =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,
          
          def draw?
           full? && won?
+            false
            end
          end
         
