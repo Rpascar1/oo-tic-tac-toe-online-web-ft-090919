cedd76e47f5bf2cab1fb42159e075438a9d06451
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-20 14:56:22 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -97,7 +97,11 @@ WIN_COMBINATIONS =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,
           end
         
          def full(board)
-           turn_count == 9
+           if turn_count == 9
+             true
+          else
+            false
+           
          end
         
         
