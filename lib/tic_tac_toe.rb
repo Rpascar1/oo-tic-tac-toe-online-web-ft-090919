73dd79c4f9adda989ede88e2293bde549e657aa3
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-20 14:50:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -91,13 +91,20 @@ WIN_COMBINATIONS =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,
           break
         else
           if @board[position1] == "O" && @board[position2] == "O" && @board[position3] =="O" 
-        won = combo
+          won = combo
           break
           end
         end
-      
-        end
       end
+      
+      
+      
+      
+      
+  end
+
+
+
 
 
 end
