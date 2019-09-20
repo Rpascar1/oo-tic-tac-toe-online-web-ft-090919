f59ea0220bb09a566962f370d60dbb54bbf97036
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-20 15:51:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -122,6 +122,7 @@ WIN_COMBINATIONS =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,
 
   def winner
       last_index = @board.map {|i| i.is_wanted? }.rindex(true)
+      last_index
   
   
   end
