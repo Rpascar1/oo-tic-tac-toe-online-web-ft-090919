12ff6e4f380ce53a1ed8a428b5db924667a544cb
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-20 13:34:56 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,12 +39,9 @@ WIN_COMBINATIONS =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,
   end
   
   def valid_move?(position)
-    if @board[position]==(" ") && (position >= 0 && position <= 8)
-      true
-    else
-      false
-    end
+  
   end
+
 
   def turn_count
     count = @board.count(" ") 
