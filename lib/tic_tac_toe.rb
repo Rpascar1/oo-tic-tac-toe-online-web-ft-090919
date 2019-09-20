4adaebdb889f213980d9b66abceedf25eab3893b
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-20 15:14:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -96,6 +96,7 @@ WIN_COMBINATIONS =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,
           break
           end
         end
+      end
   end
         
 def full?
@@ -116,15 +117,9 @@ end
         
         
         
-        end
+end
         
-      end
-      
-     
-      
-      
-      
-  end
+
 
 
 
