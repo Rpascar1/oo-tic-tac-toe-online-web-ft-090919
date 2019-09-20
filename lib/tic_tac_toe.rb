e3ddbd45b7303f9c30155a75d8993d60f910fcad
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-20 15:10:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -95,6 +95,13 @@ WIN_COMBINATIONS =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,
           won = combo
           break
           end
+          
+           def draw?
+        
+          full? && won?
+            false
+           end
+         end
         
          def full?
            if turn_count == 9
@@ -104,12 +111,7 @@ WIN_COMBINATIONS =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,
             end
          end
          
-         def draw?
         
-          full? && won?
-            false
-           end
-         end
         
         
         
