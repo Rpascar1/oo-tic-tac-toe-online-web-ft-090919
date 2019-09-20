10cb51d6512009ee5e41ddad9b53f85b79f84195
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-20 15:58:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -123,7 +123,7 @@ WIN_COMBINATIONS =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,
   def winner
     if draw? == false
       false
-    els if current_player == "X"
+    elsif current_player == "X"
     "O"
     else
     "X"
